bc4e8ff97f312ac9d1a56d16c61d113714e6db29
Wonjun Roh <rohw@oregonstate.edu>
2021-06-01 02:00:26 +0900

    Pass Test. Test if multiples of 5 prints Buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,7 +2,9 @@ def fb():
     string = "1"
     i = 2
     while i <= 100:
-        if i % 3 == 0:
+        if i % 5 == 0:
+            string += ", Buzz"
+        elif i % 3 == 0:
             string += ", Fizz"
         else:
             string += ", " + str(i)
