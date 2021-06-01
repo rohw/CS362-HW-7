9365cf8ff93ae35db67287d65450e24d72bb4f97
Wonjun Roh <rohw@oregonstate.edu>
2021-06-01 11:00:56 +0900

    Wrote test that fail. leap year except divisible by 100

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,6 +6,9 @@ class SimpleTest(unittest.TestCase):
     def test1(self):
         self.assertEqual(leapyear.ly(4), "It is a leap year")
 
+    def test2(self):
+        self.assertEqual(leapyear.ly(100), "It is not a leap year")
+
 
 if __name__ == '__main__':
     unittest.main()
