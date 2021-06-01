088915dfb0fba9a6cbad7dcf725abc882ec8e0f6
Wonjun Roh <rohw@oregonstate.edu>
2021-06-01 11:01:52 +0900

    Pass test. leap year except divisible by 100

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,8 @@
 def ly(year):
     if year % 4 == 0:
-        return "It is a leap year"
+        if year % 100 == 0:
+            return "It is not a leap year"
+        else:
+            return "It is a leap year"
     else:
         return "It is not a leap year"
