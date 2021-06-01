6b47d0242b394bd51a2876714439ba0f4a754e88
Wonjun Roh <rohw@oregonstate.edu>
2021-06-01 11:04:55 +0900

    Pass test. leap yaer if divisible by 400

--- leapyear.py
+++ leapyear.py
@@ -1,7 +1,10 @@
 def ly(year):
     if year % 4 == 0:
         if year % 100 == 0:
-            return "It is not a leap year"
+            if year % 400 == 0:
+                return "It is a leap year"
+            else:
+                return "It is not a leap year"
         else:
             return "It is a leap year"
     else:
